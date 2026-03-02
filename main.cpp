8870f297660971586be6accc83d8458bb06c46c7
Mocsee <nyaridavid10@gmail.com>
2026-03-02 19:56:09 +0100

    Hibak javitva és kommentelve

--- main.cpp
+++ main.cpp
@@ -4,23 +4,40 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    // Rossz volt a nev (N_ELEMENTS kell)
+    int *b = new int[N_ELEMENTS];
+
+    // Idezojelek es pontosvesszo potolva
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+
+    // A ciklusbol hianyzott a feltetel es a leptetes
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+
+    // A feltetel javitva, hogy lefusson a kiiras
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+
+    // Atlag nullazasa az elejen
+    int atlag = 0;
+
+    // Pontosvesszo a vesszo helyett a ciklusban
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
+
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    // Memoria felszabaditasa
+    delete[] b;
+
     return 0;
 }
